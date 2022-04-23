9d0f14b0dab4b9a6a2444d06b0ded6604be60b28
nggar <enggarjpx@gmail.com>
2022-04-23 11:48:43 +0700

    initialize repo

---
+++ index.js
@@ -0,0 +1,24 @@
+// Reverses a string.
+function reverse( string ) {
+     return Array.from( string ).reverse().join( "" );
+}
+
+// Defines a Phrase object
+function Phrase( content ) {
+     this.content = content;
+
+     // Return string to lower case
+     this.processor = function ( string ) {
+          return string.toLowerCase();
+     }
+
+     // Return content processed for palindrome testing
+     this.processedContent = function processedContent() {
+          return this.processor( this.content );
+     }
+
+     // Return true for palindrome, false otherwise.
+     this.palindrome = function palindrome() {
+          return this.processedContent() === reverse( this.processedContent() );
+     }
+}
