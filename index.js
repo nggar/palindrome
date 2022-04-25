fe888dc39569bb5c537ea59694309174e7fe5297
nggar <enggarjpx@gmail.com>
2022-04-25 11:01:34 +0700

    finish working and refactored palindrome methode

--- index.js
+++ index.js
@@ -16,13 +16,7 @@ function Phrase( content ) {
 
      // Return only letters in the content.
      this.letters = function letters() {
-          let theLetters = [];
-          for ( let i = 0; i < this.content.length; i++ ) {
-               if ( this.content.charAt( i ).match( /[a-zA-Z]/ ) ) {
-                    theLetters.push( this.content.charAt( i ) );
-               }
-          }
-          return theLetters.join( "" );
+          return ( this.content.match( /[a-z]/ig ) || [] ).join( "" );
      }
 
      // Return true for palindrome, false otherwise.
